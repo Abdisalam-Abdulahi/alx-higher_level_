fcf05aaab6784752d677918748c86e1e75b3240f
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-11-03 08:38:44 +0300

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+"""
+Reactangele class
+"""
+
+
+class Rectangle:
+    """
+    Reactange class with width and height
+    """
+
+
+    def __init__(self, width=0, height=0):
+        """
+        initialize
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        getter function
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        setter function
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """
+        getter function
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        setter function
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__height = value
+
+
+
+
+
