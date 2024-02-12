bc35a3d35e1b1df27813461c004b68515ca4e2ae
Abdisalam-Abdulahi <iidlex99@gmail.com>
2024-02-12 15:44:45 +0300

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
- """
+"""
  this module ....
  reades the content of the file
  """
 
- def read_file(filename=""):
-     """
-     reades the content of the file
-     """
-     with open("filename", "r","utf-8") as file:
-         print(file.read())
 
+def read_file(filename=""):
+    """
+    reades the content of the file
+    """
+    with open("filename", "r", "utf-8") as file:
+        print(file.read())
