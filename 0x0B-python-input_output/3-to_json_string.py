c9aad3ea7bfff97e0e02d5579321401b5283eb98
Abdisalam-Abdulahi <iidlex99@gmail.com>
2024-02-13 07:48:40 +0000

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import json
+
+"""
+converts object to json string
+"""
+
+
+def to_json_string(my_obj):
+    """
+    function that returns JSON representation of an object
+    """
+    return json.dumps(my_obj)
